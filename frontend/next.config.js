c3d2db619b548320d882a5c684cf43a2c5687dcc
Doug Kulak <doug.kulak@gmail.com>
2021-07-22 17:39:58 -0700

    Upgrading next to latest

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,6 +1,15 @@
 // next.config.js
-const withImages = require('next-images')
-module.exports = withImages({env: {
-    REACT_APP_MAPBOX_TOKEN: process.env.REACT_APP_MAPBOX_TOKEN,
-        DIRECTUS_ENDPOINT: process.env.DIRECTUS_ENDPOINT,
-}})
+require('dotenv').config()
+const withImages = require('next-images');
+module.exports = withImages(
+  {
+    env: {
+      REACT_APP_MAPBOX_TOKEN: process.env.REACT_APP_MAPBOX_TOKEN,
+      DIRECTUS_ENDPOINT: process.env.DIRECTUS_ENDPOINT,
+    },
+    webpack5: true,
+    eslint: {
+      ignoreDuringBuilds: true,
+    },
+  }
+);
